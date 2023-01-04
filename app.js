0433b8e2ecea025a35fe54d8241676fd5ad50910
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-01-04 21:05:58 +0530

    04/01/2023 1024px Desktop

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ links.forEach((link) => {
             case "Speedy Searching":
                 secImg.innerHTML = `
             <img class="tab-img" src="./images/illustration-features-tab-2.svg" alt="hero-img">
-            <div class="tab-img-illustration"></div>
+            <div class="tab-img-illustration-two"></div>
             `
                 heading.textContent = 'Intelligent search'
                 para.textContent = 'Our powerful search feature will help you find saved sites in no time at all.  No need to trawl through all of your bookmarks.'
@@ -47,7 +47,7 @@ links.forEach((link) => {
             case "Easy Sharing":
                 secImg.innerHTML = `
                     <img class="tab-img" src="./images/illustration-features-tab-3.svg" alt="hero-img">
-                    <div class="tab-img-illustration"></div>
+                    <div class="tab-img-illustration-three"></div>
                     `
                 heading.textContent = 'Share your bookmarks'
                 para.textContent = ' Easily share your bookmarks and collections with others. Create a shareable  link that you can send at the click of a button.'
@@ -60,49 +60,55 @@ links.forEach((link) => {
     })
 })
 
+console.log(window.innerWidth)
+
 toggle.forEach((btn, i) => {
     btn.addEventListener("click", () => {
         if (parseInt(content[i].style.height) != content[i].scrollHeight) {
             content[i].style.height = content[1].scrollHeight + "px"
             if (faqPText[i].textContent.length <= 250) {
                 content[i].style.height = content[1].scrollHeight - 70 + "px"
+                if( window.innerWidth >= 580){
+                    content[i].style.height = content[1].scrollHeight - 20 + "px"
+                }
             }
             arrowIcon[i].classList.add('rotate-arrow-icon')
-        } else {
+        } 
+        
+        else {
             content[i].style.height = "0px"
             arrowIcon[i].classList.remove('rotate-arrow-icon')
         }
     })
 })
 
-From.addEventListener("submit", (e)=>{
+From.addEventListener("submit", (e) => {
 
     e.preventDefault();
 
-   const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if(!Input.value.match(mailformat)){
-        error.classList.add('show-error');  
-        errorMsg.classList.add('show-error-msg');  
+    if (!Input.value.match(mailformat)) {
+        error.classList.add('show-error');
+        errorMsg.classList.add('show-error-msg');
         Input.style.border = "2px solid #fa5757"
-        From.style.gap="2.1rem"
-    return true;
+        From.style.gap = "2.1rem"
+        return true;
     }
     else {
-        error.classList.remove('show-error');  
-        errorMsg.classList.remove('show-error-msg');  
+        error.classList.remove('show-error');
+        errorMsg.classList.remove('show-error-msg');
         Input.style.border = "none"
-        From.style.gap="1.1rem"
-    return false;
+        From.style.gap = "1.1rem"
+        return false;
     }
-    
-    });
-    
 
-open.addEventListener("click", () => { 
- sideMenu.classList.add('open-side-menu')
- close.addEventListener("click", () => {
- sideMenu.classList.remove('open-side-menu')
+});
 
-  })
+
+open.addEventListener("click", () => {
+    sideMenu.classList.add('open-side-menu')
+    close.addEventListener("click", () => {
+        sideMenu.classList.remove('open-side-menu')
+    })
 })
